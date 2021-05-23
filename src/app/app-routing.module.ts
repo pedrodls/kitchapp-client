4511c1d3a4c46ad4a65472e11954d0e0fbf5f295
PedroSousa2020 <sousapedro747@gmail.com>
2021-05-23 23:09:09 +0100

    fix conflit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { FrontofficeModule } from './frontoffice/frontoffice.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    FrontofficeModule
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
